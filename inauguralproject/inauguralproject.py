b0f51c6f6e752b7c929f11fddd8fe401fcbe8f1e
lkh927 <lkh927@alumni.ku.dk>
2024-03-19 20:21:10 +0100

    Meeting tuesday

--- inauguralproject/inauguralproject.py
+++ inauguralproject/inauguralproject.py
@@ -1,5 +1,12 @@
 from types import SimpleNamespace
 
+import numpy as np
+from scipy import optimize
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
 class ExchangeEconomyClass:
 
     def __init__(self):
@@ -15,19 +22,26 @@ class ExchangeEconomyClass:
         par.w2A = 0.3
 
     def utility_A(self,x1A,x2A):
-        pass
+        par = self.par
+        return x1A**par.alpha * x2A**(1-par.alpha)
 
     def utility_B(self,x1B,x2B):
-        pass
+        par = self.par
+        return x1B**par.beta * x2B**(1-par.beta)
 
     def demand_A(self,p1):
-        pass
-
+        par = self.par
+        x1A = par.alpha*(p1*par.w1A+par.w2A)/p1
+        x2A = (1-par.alpha)*(p1*par.w1A+par.w2A)
+        return x1A, x2A
+    
     def demand_B(self,p1):
-        pass
-
+        par = self.par
+        x1B = par.beta*((p1*(1-par.w1A))+(1-par.w2A))/p1
+        x2B = (1-par.beta)*(p1*(1-par.w1A)+(1-par.w2A))
+        return x1B, x2B
+       
     def check_market_clearing(self,p1):
-
         par = self.par
 
         x1A,x2A = self.demand_A(p1)
@@ -37,3 +51,31 @@ class ExchangeEconomyClass:
         eps2 = x2A-par.w2A + x2B-(1-par.w2A)
 
         return eps1,eps2
+
+    def market_clearing_error(self):
+        N = 75
+        p1_vec = np.linspace(0.5,2.5,N)
+        eps1_values = np.empty(N)
+        eps2_values = np.empty(N)
+
+        for p1 in p1_vec:
+            eps1_values, eps2_values = self.check_market_clearing(p1)
+        return eps1_values, eps2_values
+
+    def pareto_improvement(self):
+        par = self.par
+
+        cons_x1A = []
+        cons_x2A = []
+
+        x1A_vec = np.linspace(0,1,N)
+        x2A_vec = np.linspace(0,1,N)
+
+        for x1A in (x1A_vec):
+            for x2A in (x2A_vec):
+                if (self.utility_A(x1A, x2A) >= self.utility_A(par.w1A, par.w2A)) and (self.utility_B(1-x1A, 1-x1A)) >= self.utility_B(1-par.w1A, 1-par.w2A):
+                    cons_x1A.append(x1A)
+                    cons_x2A.append(x2A)
+        
+        return 
+
