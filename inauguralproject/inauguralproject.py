65481af91d358940d368e01e773916114f43eeb5
annaabildskov <bvh632@alumni.ku.dk>
2024-03-23 23:44:42 +0100

    anna 23.3

--- inauguralproject/inauguralproject.py
+++ inauguralproject/inauguralproject.py
@@ -103,6 +103,9 @@ class ExchangeEconomyClass:
     def price_setter(self,p1):
         '''objective function to use for maximizing (minimizing)'''
         par = self.par
-        return -par.utility_A(1-par.x1B, 1-par.x2B)
+        x1B,x2B = self.demand_B(p1)
+        return -self.utility_A(1-x1B, 1-x2B)
+
+
         
     
