2b88e0b1fec64cb65d3da35b5bf0f031bbc4d5ae
lkh927 <lkh927@alumni.ku.dk>
2024-04-11 18:49:43 +0200

    ikke kommet nogen fucking vejne

--- dataproject/dataproject.py
+++ dataproject/dataproject.py
@@ -14,9 +14,25 @@ import dataproject
 import pandas_datareader # install with `pip install pandas-datareader`
 from dstapi import DstApi # install with `pip install git+https://github.com/alemartinello/dstapi`
 
-class DstAPI:
-    def __init__(self):
-        par = self.par = SimpleNamespace()
-    
-    def importdata(self):
+# importing the actual data from DST
+employees = DstApi('LBESK03')
+lb_short_service = DstApi('KBS2')
+lb_short_manu = DstApi('BARO3')
+lb_short_cons = DstApi('KBYG33')
+with open('International Labor.json', 'r') as f:
+    int_data = json.load(f)
+int_lb = pd.DataFrame(int_data)
+
+# cleaning the 'LBESK03' dataset
+params = employees._define_base_params(language='en')
+
+params = {'table': 'LBESK03',
+ 'format': 'BULK',
+ 'lang': 'en',
+ 'variables': [{'code': 'BRANCHEDB071038', 'values': ['TOT']},
+  {'code': 'Tid', 'values': ['>2013M12<=2024M01']}]}
+
+empl = employees.get_data(params=params)
+empl.head(5)
+
 
