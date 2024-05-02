d564c33cc957aab4c8c5126da75af90608d51ad0
lkh927 <lkh927@alumni.ku.dk>
2024-05-02 10:22:16 +0200

    påbegyndt class

--- modelproject/modelproject.py
+++ modelproject/modelproject.py
@@ -1,22 +1,39 @@
 from scipy import optimize
+from types import SimpleNamespace
 
-def solve_ss(alpha, c):
-    """ Example function. Solve for steady state k. 
 
-    Args:
-        c (float): costs
-        alpha (float): parameter
+class CoutnotOligopoly:
+    """ This class implements the Cournot oligopoly model"""
+    def __init__(self):
+        par = self.par = SimpleNamespace()
+        par.a = a
+        par.b = b
+        par.c = c
 
-    Returns:
-        result (RootResults): the solution represented as a RootResults object.
 
-    """ 
+    def invdemand(self,q1,q2):
+        par = self.par
+        p = a - b*(q1+q2)
+        return p
     
-    # a. Objective function, depends on k (endogenous) and c (exogenous).
-    f = lambda k: k**alpha - c
-    obj = lambda kss: kss - f(kss)
+    def cost(self,qi):
+        par = self.par
+        cost = c*qi
+        return cost
 
-    #. b. call root finder to find kss.
-    result = optimize.root_scalar(obj,bracket=[0.1,100],method='bisect')
+    def profit1(self,q1):
+        par = self.par
+        p = self.invdemand()
+        cost1 = self.cost(q1)
+        profit1 = p*q1 - cost1
+        return profit1
+
+    def profit2(self,q2):
+        par = self.par
+        p = self.invdemand()
+        cost2 = self.cost(q2)
+        profit2 = p*q2 - cost2
+        return profit2
+
+
     
-    return result
